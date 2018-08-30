aed9282592713ee425a626b38c0b726851feaa58
xrr2016 <xiaoranran1993@163.com>
2018-08-31 06:18:49 +0800

    :zap: dev dashboard

--- cake-shop-admin/src/layouts/Dashboard/index.js
+++ cake-shop-admin/src/layouts/Dashboard/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { Layout, Menu, Icon } from 'antd'
+import { Layout, Menu, Icon, Avatar, Dropdown } from 'antd'
 import './style.css'
 
-const { Header, Sider, Content, Footer } = Layout
+const { Header, Sider, Content } = Layout
+const { SubMenu } = Menu
 
 export default class Dashboard extends Component {
   state = {
@@ -13,144 +14,129 @@ export default class Dashboard extends Component {
 
   render() {
     const { collapsed } = this.state
+    const headerDropdownMenu = (
+      <Menu>
+        <Menu.Item key="0">
+          <Icon type="user" />
+          <span>个人中心</span>
+        </Menu.Item>
+        <Menu.Item key="1">
+          <Icon type="setting" />
+          <span>设置</span>
+        </Menu.Item>
+        <Menu.Divider />
+        <Menu.Item key="3">
+          <Icon type="logout" />
+          <span>退出登录</span>
+        </Menu.Item>
+      </Menu>
+    )
     return (
       <Layout className="dashboard">
-        <Sider className="dashboard__sider" trigger={null} collapsed collapsed={collapsed}>
-          <div className="dashboard__logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} defaultOpenKeys={['1']}>
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span>nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span>nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span>nav 3</span>
-            </Menu.Item>
+        <Sider
+          className="dashboard__sider"
+          width="256"
+          trigger={null}
+          theme="dark"
+          collapsed={collapsed}
+        >
+          <h1 className="dashboard__logo">
+            <Icon className="logo" type="setting" />
+            <span className="title">Cake Shop Admin</span>
+          </h1>
+          <Menu mode="inline" theme="dark" defaultSelectedKeys={['0']}>
+            <SubMenu
+              key="sun0"
+              title={
+                <span>
+                  <Icon type="user" />
+                  <span>Navigation One</span>
+                </span>
+              }
+            >
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="mail" />
+                  <span>Navigation One</span>
+                </span>
+              }
+            >
+              <Menu.Item key="5">Option 5</Menu.Item>
+              <Menu.Item key="6">Option 6</Menu.Item>
+              <Menu.Item key="7">Option 7</Menu.Item>
+              <Menu.Item key="8">Option 8</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="mail" />
+                  <span>Navigation One</span>
+                </span>
+              }
+            >
+              <Menu.Item key="5">Option 5</Menu.Item>
+              <Menu.Item key="6">Option 6</Menu.Item>
+              <Menu.Item key="7">Option 7</Menu.Item>
+              <Menu.Item key="8">Option 8</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub3"
+              title={
+                <span>
+                  <Icon type="mail" />
+                  <span>Navigation One</span>
+                </span>
+              }
+            >
+              <Menu.Item key="5">Option 5</Menu.Item>
+              <Menu.Item key="6">Option 6</Menu.Item>
+              <Menu.Item key="7">Option 7</Menu.Item>
+              <Menu.Item key="8">Option 8</Menu.Item>
+            </SubMenu>
           </Menu>
         </Sider>
-        <Layout>
-          <Header className="dashboard__header" style={{ background: '#fff', padding: 0 }}>
+        <Layout className="dashboard__main">
+          <Header className="dashboard__header">
             <Icon
               className="dashboard__trigger"
               type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
               onClick={this.toggle}
             />
+            <div className="dashboard__actions">
+              <span className="dashboard__action">
+                <Icon type="question-circle-o" />
+              </span>
+              <Dropdown overlay={headerDropdownMenu}>
+                <span className="dashboard__action">
+                  <Avatar
+                    className="dashboard__avatar"
+                    style={{ backgroundColor: '#87d068' }}
+                    icon="user"
+                  />
+                  <span className="dashboard__username">xrr2016</span>
+                </span>
+              </Dropdown>
+            </div>
           </Header>
-          <Content
-            className="dashboard__content"
-            style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}
-          >
-            Content Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat ullam, quis
-            corrupti reiciendis ad ipsam omnis impedit natus qui fugiat rem eveniet id animi,
-            laudantium culpa totam? Ex, illum aliquid? Lorem, ipsum dolor sit amet consectetur
-            adipisicing elit. Magnam doloribus veritatis asperiores deleniti laudantium eum minus.
-            Nulla impedit, amet laudantium corporis cum quibusdam, officia suscipit aliquid maxime
-            doloremque voluptatem distinctio. Lorem ipsum dolor sit amet consectetur, adipisicing
-            elit. Eaque iste facere repudiandae aspernatur, cum nulla asperiores vel hic nemo qui
-            doloremque. Illo quam ipsum quos! Aliquam facere impedit voluptatibus sit. Lorem ipsum
-            dolor sit amet consectetur, adipisicing elit. Culpa ipsa iste iure non nostrum sunt
-            minus, consequatur fugit facilis quas delectus ea. Maxime odio praesentium reiciendis
-            quia quibusdam nam commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dolorum at suscipit repellat labore provident alias repudiandae, veniam similique
-            aliquam velit illo minus delectus sapiente beatae atque perspiciatis molestias!
-            Temporibus, nisi! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat
-            ullam, quis corrupti reiciendis ad ipsam omnis impedit natus qui fugiat rem eveniet id
-            animi, laudantium culpa totam? Ex, illum aliquid? Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Magnam doloribus veritatis asperiores deleniti laudantium
-            eum minus. Nulla impedit, amet laudantium corporis cum quibusdam, officia suscipit
-            aliquid maxime doloremque voluptatem distinctio. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Eaque iste facere repudiandae aspernatur, cum nulla asperiores vel hic
-            nemo qui doloremque. Illo quam ipsum quos! Aliquam facere impedit voluptatibus sit.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa ipsa iste iure non
-            nostrum sunt minus, consequatur fugit facilis quas delectus ea. Maxime odio praesentium
-            reiciendis quia quibusdam nam commodi. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Dolorum at suscipit repellat labore provident alias repudiandae,
-            veniam similique aliquam velit illo minus delectus sapiente beatae atque perspiciatis
-            molestias! Temporibus, nisi! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Quaerat ullam, quis corrupti reiciendis ad ipsam omnis impedit natus qui fugiat rem
-            eveniet id animi, laudantium culpa totam? Ex, illum aliquid? Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Magnam doloribus veritatis asperiores deleniti laudantium
-            eum minus. Nulla impedit, amet laudantium corporis cum quibusdam, officia suscipit
-            aliquid maxime doloremque voluptatem distinctio. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Eaque iste facere repudiandae aspernatur, cum nulla asperiores vel hic
-            nemo qui doloremque. Illo quam ipsum quos! Aliquam facere impedit voluptatibus sit.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa ipsa iste iure non
-            nostrum sunt minus, consequatur fugit facilis quas delectus ea. Maxime odio praesentium
-            reiciendis quia quibusdam nam commodi. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Dolorum at suscipit repellat labore provident alias repudiandae,
-            veniam similique aliquam velit illo minus delectus sapiente beatae atque perspiciatis
-            molestias! Temporibus, nisi! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Quaerat ullam, quis corrupti reiciendis ad ipsam omnis impedit natus qui fugiat rem
-            eveniet id animi, laudantium culpa totam? Ex, illum aliquid? Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Magnam doloribus veritatis asperiores deleniti laudantium
-            eum minus. Nulla impedit, amet laudantium corporis cum quibusdam, officia suscipit
-            aliquid maxime doloremque voluptatem distinctio. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Eaque iste facere repudiandae aspernatur, cum nulla asperiores vel hic
-            nemo qui doloremque. Illo quam ipsum quos! Aliquam facere impedit voluptatibus sit.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa ipsa iste iure non
-            nostrum sunt minus, consequatur fugit facilis quas delectus ea. Maxime odio praesentium
-            reiciendis quia quibusdam nam commodi. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Dolorum at suscipit repellat labore provident alias repudiandae,
-            veniam similique aliquam velit illo minus delectus sapiente beatae atque perspiciatis
-            molestias! Temporibus, nisi! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Quaerat ullam, quis corrupti reiciendis ad ipsam omnis impedit natus qui fugiat rem
-            eveniet id animi, laudantium culpa totam? Ex, illum aliquid? Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Magnam doloribus veritatis asperiores deleniti laudantium
-            eum minus. Nulla impedit, amet laudantium corporis cum quibusdam, officia suscipit
-            aliquid maxime doloremque voluptatem distinctio. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Eaque iste facere repudiandae aspernatur, cum nulla asperiores vel hic
-            nemo qui doloremque. Illo quam ipsum quos! Aliquam facere impedit voluptatibus sit.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa ipsa iste iure non
-            nostrum sunt minus, consequatur fugit facilis quas delectus ea. Maxime odio praesentium
-            reiciendis quia quibusdam nam commodi. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Dolorum at suscipit repellat labore provident alias repudiandae,
-            veniam similique aliquam velit illo minus delectus sapiente beatae atque perspiciatis
-            molestias! Temporibus, nisi! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Quaerat ullam, quis corrupti reiciendis ad ipsam omnis impedit natus qui fugiat rem
-            eveniet id animi, laudantium culpa totam? Ex, illum aliquid? Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Magnam doloribus veritatis asperiores deleniti laudantium
-            eum minus. Nulla impedit, amet laudantium corporis cum quibusdam, officia suscipit
-            aliquid maxime doloremque voluptatem distinctio. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Eaque iste facere repudiandae aspernatur, cum nulla asperiores vel hic
-            nemo qui doloremque. Illo quam ipsum quos! Aliquam facere impedit voluptatibus sit.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa ipsa iste iure non
-            nostrum sunt minus, consequatur fugit facilis quas delectus ea. Maxime odio praesentium
-            reiciendis quia quibusdam nam commodi. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Dolorum at suscipit repellat labore provident alias repudiandae,
-            veniam similique aliquam velit illo minus delectus sapiente beatae atque perspiciatis
-            molestias! Temporibus, nisi! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Quaerat ullam, quis corrupti reiciendis ad ipsam omnis impedit natus qui fugiat rem
-            eveniet id animi, laudantium culpa totam? Ex, illum aliquid? Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Magnam doloribus veritatis asperiores deleniti laudantium
-            eum minus. Nulla impedit, amet laudantium corporis cum quibusdam, officia suscipit
-            aliquid maxime doloremque voluptatem distinctio. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Eaque iste facere repudiandae aspernatur, cum nulla asperiores vel hic
-            nemo qui doloremque. Illo quam ipsum quos! Aliquam facere impedit voluptatibus sit.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa ipsa iste iure non
-            nostrum sunt minus, consequatur fugit facilis quas delectus ea. Maxime odio praesentium
-            reiciendis quia quibusdam nam commodi. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Dolorum at suscipit repellat labore provident alias repudiandae,
-            veniam similique aliquam velit illo minus delectus sapiente beatae atque perspiciatis
-            molestias! Temporibus, nisi! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Quaerat ullam, quis corrupti reiciendis ad ipsam omnis impedit natus qui fugiat rem
-            eveniet id animi, laudantium culpa totam? Ex, illum aliquid? Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Magnam doloribus veritatis asperiores deleniti laudantium
-            eum minus. Nulla impedit, amet laudantium corporis cum quibusdam, officia suscipit
-            aliquid maxime doloremque voluptatem distinctio. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Eaque iste facere repudiandae aspernatur, cum nulla asperiores vel hic
-            nemo qui doloremque. Illo quam ipsum quos! Aliquam facere impedit voluptatibus sit.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa ipsa iste iure non
-            nostrum sunt minus, consequatur fugit facilis quas delectus ea. Maxime odio praesentium
-            reiciendis quia quibusdam nam commodi. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Dolorum at suscipit repellat labore provident alias repudiandae,
-            veniam similique aliquam velit illo minus delectus sapiente beatae atque perspiciatis
-            molestias! Temporibus, nisi!
+          <Content className="dashboard__content">
+            uscipit aliquid maxime doloremque voluptatem distinctio. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Eaque iste facere repudiandae aspernatur, cum nulla
+            asperiores vel hic nemo qui doloremque. Illo quam ipsum quos! Aliquam facere impedit
+            voluptatibus sit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa ipsa
+            iste iure non nostrum sunt minus, consequatur fugit facilis quas delectus ea. Maxime
+            odio praesentium reiciendis quia quibusdam nam commodi. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Dolorum at suscipit repellat labore provident alias
+            repudiandae, veniam similique aliquam velit illo minus delectus sapiente beatae atque
+            perspiciatis molestias! Temporibus, nisi!
           </Content>
-          <Footer style={{ textAlign: 'center' }}>xrr2016</Footer>
         </Layout>
       </Layout>
     )
